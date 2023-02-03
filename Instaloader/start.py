a1798f4791429b81c615e27a6ca930113e18408a
bigg02 <122289263+bigg02@users.noreply.github.com>
2023-02-03 20:50:55 +0530

    Update start.py

--- Instaloader/start.py
+++ Instaloader/start.py
@@ -6,10 +6,10 @@ from pyrogram.types import InlineKeyboardMarkup
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, msg):
-	user = await bot.get_me()
-	mention = user["mention"]
+#	user = await bot.get_me()
+#	mention = user["mention"]
 	await bot.send_message(
 		msg.chat.id,
-		Data.START.format(msg.from_user.mention, mention),
+		Data.START.format(msg.from_user),
 		reply_markup=InlineKeyboardMarkup(Data.buttons)
 	)
